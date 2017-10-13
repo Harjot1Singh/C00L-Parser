0bb7c01131159a4e21df84950a202339226ae505
Harjot Singh <harjot@harkul.com>
2017-10-14 00:20:38 +0100

    basic logger usage

--- src/coolparser.py
+++ src/coolparser.py
@@ -1,8 +1,19 @@
+import logger
 
+
+# Parser entry point
 def parse(path):
+    logger.header('Parsing', path)
+    try:
+        # Lex
+        # Parse
+        logger.success('No errors found in', path)
+    except Exception:
+        logger.error('Errors found in', path)
     pass
 
 
+# Application entry point
 if __name__ == "__main__":
     import sys
     parse(sys.argv[1])
