a48ee5cfbec100303768a836257e810c646da979
Harjot Singh <harjot@harkul.com>
2017-11-03 00:50:34 +0000

    linux line endings

--- test-cool.sh
+++ test-cool.sh
@@ -2,6 +2,7 @@
 
 for file in tests/*
 do
-    echo -e "\nRunning parser on ${file}"
-    python src/coolparser.py ${file}
+	echo -e "\nRunning parser on ${file}"
+	python src/coolparser.py ${file}
 done
+
