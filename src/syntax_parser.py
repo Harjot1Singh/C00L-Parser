1a12bbcc9f447655c34bae10ad34c5b259e73f68
Harjot Singh <harjot@harkul.com>
2017-11-04 20:13:41 +0000

    add parse boilerplate

--- src/syntax_parser.py
+++ src/syntax_parser.py
@@ -38,6 +38,10 @@ def parse(token_lst):
     return [], []
 
 
+def parse_predict(predict_dict):
+    pass
+
+
 def parse_program(token_iter):
     token = token_iter.next()
 
@@ -52,5 +56,150 @@ def parse_program(token_iter):
         print('not matched')
 
 
-def parse_class(token_iter):
-    token = token_iter.next()
+def parse_programs(token_iter):
+    pass
+
+
+def parse_class_def(token_iter):
+    pass
+
+
+def parse_class_detail(token_iter):
+    pass
+
+
+def parse_bracket_feature(token_iter):
+    pass
+
+
+def parse_optional_bracket_feature(token_iter):
+    pass
+
+
+def parse_class_feature(token_iter):
+    pass
+
+
+def parse_class_features(token_iter):
+    pass
+
+
+def parse_feature(token_iter):
+    pass
+
+
+def parse_feature_details(token_iter):
+    pass
+
+
+def parse_optional_features(token_iter):
+    pass
+
+
+def parse_optional_expression(token_iter):
+    pass
+
+
+def parse_feature_formal(token_iter):
+    pass
+
+
+def parse_feature_formals(token_iter):
+    pass
+
+
+def parse_formal(token_iter):
+    pass
+
+
+def parse_expr(token_iter):
+    pass
+
+
+def parse_expr_rr(token_iter):
+    pass
+
+
+def parse_id_type_arrow(token_iter):
+    pass
+
+
+def parse_id_type_arrows(token_iter):
+    pass
+
+
+def parse_id_type_expr(token_iter):
+    pass
+
+
+def parse_id_type_exprs(token_iter):
+    pass
+
+
+def parse_optional_comma_expr(token_iter):
+    pass
+
+
+def parse_comma_expr(token_iter):
+    pass
+
+
+def parse_comma_exprs(token_iter):
+    pass
+
+
+def parse_semicolon_expr(token_iter):
+    pass
+
+
+def parse_semicolon_exprs(token_iter):
+    pass
+
+
+def parse_assign_term(token_iter):
+    pass
+
+
+def parse_not_term(token_iter):
+    pass
+
+
+def parse_compare_term(token_iter):
+    pass
+
+
+def parse_compare_term_rr(token_iter):
+    pass
+
+
+def parse_add_term(token_iter):
+    pass
+
+
+def parse_add_term_rr(token_iter):
+    pass
+
+
+def parse_multi_term(token_iter):
+    pass
+
+
+def parse_multi_term_rr(token_iter):
+    pass
+
+
+def parse_isvoid_term(token_iter):
+    pass
+
+
+def parse_tilde_term(token_iter):
+    pass
+
+
+def parse_factor(token_iter):
+    pass
+
+
+def parse_factor_id(token_iter):
+    pass
+
