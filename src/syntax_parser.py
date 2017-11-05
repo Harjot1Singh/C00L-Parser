86970d5bec5767b7008f0c045879801fe705107c
Harjot Singh <harjot@harkul.com>
2017-11-05 03:21:24 +0000

    annotated and removed repeated code

--- src/syntax_parser.py
+++ src/syntax_parser.py
@@ -1,7 +1,7 @@
 import tokens
 
 
-# Iterator wrapper that allows peeking
+# Iterator wrapper that allows looking ahead at the next item
 class PeekableIterator:
     def __init__(self, lst):
         self.iterator = iter(lst)
@@ -33,173 +33,351 @@ def parse(token_lst):
     # Setup iterator with lookahead functionality
     iterator = PeekableIterator(token_lst)
 
-    parse_program(iterator)
+    program(iterator)
 
     return [], []
 
 
-def parse_predict(predict_dict):
-    pass
+def parse_next(predict_dict, token_iter):
+    next_token = token_iter.lookahead()
+
+    for predict_set in predict_dict:
+        try:
+            # Check if the token is in the set
+            next(required_token for required_token in predict_set if compare_token(next_token, required_token))
+
+            # Pull next productions as list of expected tokens
+            production = predict_dict[predict_set]
+
+            # Do stuff with empty productions
+            if production is None:
+                token_iter.next()
+                return
+
+            for terminal in production:
+                if isinstance(terminal, tuple):
+                    token = token_iter.next()
+
+                    if not compare_token(token, terminal):
+                        print('oh no, expected', terminal, 'got', token)
+                else:
+                    terminal(token_iter)
+
+            return
+        except StopIteration:
+            continue
+
+    print('No match found wtf raise an error')
 
 
-def parse_program(token_iter):
-    token = token_iter.next()
+# Compares a token to a required token
+# required is a tuple of (tokenType, requiredValue)
+def compare_token(token, required):
+    required_token_type, required_value = required
 
-    if isinstance(token, tokens.KeywordToken) and token.val() == 'class':
-        parse_program(token_iter)
-        token = token_iter.next()
-
-        if isinstance(token, tokens.SemiColonToken):
-            parse_program(token_iter)
-
+    if required_value is None:
+        return isinstance(token, required_token_type)
     else:
-        print('not matched')
+        return isinstance(token, required_token_type) and token.val() == required_value
+
+
+def parse_next_decorate(production):
+    def wrapper(token_iter):
+        return parse_next(production(), token_iter)
+
+    return wrapper
+
+
+#####################################################################################
+# Each of the following productions will be commented with a tabular list, denoting:#
+# Production                                                First+ Set              #
+#                                                                                   #
+# The functions all return a first+ set to production mapping, which the decorator  #
+# function then uses to consume and check the tokens                                #
+#####################################################################################
+
+# class_def ; programs                      class
+@parse_next_decorate
+def program():
+    return {
+        frozenset({(tokens.KeywordToken, 'class')}): [class_def, (tokens.SemiColonToken, None), programs]
+    }
 
 
-def parse_programs(token_iter):
-    pass
+# program                                   class
+# ε                                         $
+@parse_next_decorate
+def programs():
+    return {
+        frozenset({(tokens.KeywordToken, 'class')}): [program],
+        frozenset({(tokens.EOFToken, None)}): None
+    }
 
 
-def parse_class_def(token_iter):
-    pass
+# class TYPE class_detail                   class
+@parse_next_decorate
+def class_def():
+    return {
+        frozenset({(tokens.KeywordToken, 'class')}): [(tokens.KeywordToken, 'class'),
+                                                      (tokens.TypeIdToken, None),
+                                                      class_detail]
+    }
 
+
+# bracket_feature                           {
+# inherits TYPE bracket_feature             inherits
+@parse_next_decorate
+def class_detail():
+    return {
+        frozenset({(tokens.BracketToken, '{')}): [bracket_feature],
+        frozenset({(tokens.KeywordToken, 'inherits')}): [(tokens.KeywordToken, 'inherits'),
+                                                         (tokens.TypeIdToken, None),
+                                                         bracket_feature]
+    }
+
+
+# { optional_bracket_feature }              {
+@parse_next_decorate
+def bracket_feature():
+    return {
+        frozenset({(tokens.BracketToken, '{')}): [(tokens.BracketToken, '{'), optional_bracket_feature]
+    }
 
-def parse_class_detail(token_iter):
-    pass
 
+# class_feature	                            ID
+# ε	                                        }
+@parse_next_decorate
+def optional_bracket_feature():
+    return {
+        frozenset({(tokens.TypeIdToken, None)}): [class_feature],
+        frozenset({(tokens.BracketToken, '}')}): None
+    }
 
-def parse_bracket_feature(token_iter):
-    pass
 
+# feature ; class_features                  ID
+@parse_next_decorate
+def class_feature():
+    return {
+        frozenset({})
+    }
 
-def parse_optional_bracket_feature(token_iter):
-    pass
 
+# class_feature	                            ID
+# ε	                                        }
+@parse_next_decorate
+def class_features():
+    return {}
 
-def parse_class_feature(token_iter):
-    pass
 
+# ID feature_details                        ID
+@parse_next_decorate
+def feature():
+    return {}
 
-def parse_class_features(token_iter):
-    pass
 
+# ( optional_features ) : TYPE { expr }     (
+# : TYPE optional_expr                      :
+@parse_next_decorate
+def feature_details():
+    return {}
 
-def parse_feature(token_iter):
-    pass
 
+# feature_formal                            ID
+# ε	                                        )
+@parse_next_decorate
+def optional_features():
+    return {}
 
-def parse_feature_details(token_iter):
-    pass
 
+# <- expr                                   <-
+# ε                                         ,, in, ;
+@parse_next_decorate
+def optional_expression():
+    return {}
 
-def parse_optional_features(token_iter):
-    pass
 
+# formal feature_formals                    ID
+@parse_next_decorate
+def feature_formal():
+    return {}
 
-def parse_optional_expression(token_iter):
-    pass
 
+# , feature_formal	                        ,
+# ε                                         )
+@parse_next_decorate
+def feature_formals():
+    return {}
 
-def parse_feature_formal(token_iter):
-    pass
 
+# ID : TYPE                                 ID
+@parse_next_decorate
+def formal():
+    return {}
 
-def parse_feature_formals(token_iter):
-    pass
 
+# if expr then expr else expr fi expr_rr    if
+# while expr loop expr pool expr_rr         while
+# { semicolon_expr } expr_rr                {
+# let id_type_expr in expr expr_rr          let
+# case expr of id_type_arrow esac expr_rr   case
+# new TYPE expr_rr                          new
+# ( expr ) expr_rr                          (
+# assign_term expr_rr                       ID, not, isvoid, ~, integer, string, true, false
+@parse_next_decorate
+def expr():
+    return {}
 
-def parse_formal(token_iter):
-    pass
 
+# @TYPE.ID( optional_comma_expr ) expr_rr   @
+# . ID ( optional_comma_expr ) expr_rr      .
+# ε                                         ;, ,, then, else, fi, loop, pool, @, ., of, ), }, in, *, /, +, -, <=, <, =
+@parse_next_decorate
+def expr_rr():
+    return {}
 
-def parse_expr(token_iter):
-    pass
 
+# formal => expr ; id_type_arrows           ID
+@parse_next_decorate
+def id_type_arrow():
+    return {}
 
-def parse_expr_rr(token_iter):
-    pass
 
+# id_type_arrow                             ID
+# ε                                         esac
+@parse_next_decorate
+def id_type_arrows():
+    return {}
 
-def parse_id_type_arrow(token_iter):
-    pass
 
+# formal optional_expr id_type_exprs        ID
+@parse_next_decorate
+def id_type_expr():
+    return {}
 
-def parse_id_type_arrows(token_iter):
-    pass
 
+# , id_type_expr                            ,
+# ε                                         in
+@parse_next_decorate
+def id_type_exprs():
+    return {}
 
-def parse_id_type_expr(token_iter):
-    pass
 
+# comma_expr                        if, while, {, let, case, new, (, ID, not, isvoid, ~, integer, string, true, false
+# ε                                 )
+@parse_next_decorate
+def optional_comma_expr():
+    return {}
 
-def parse_id_type_exprs(token_iter):
-    pass
 
+# expr comma_exprs                  if, while, {, let, case, new, (, ID, not, isvoid, ~, integer, string, true, false
+@parse_next_decorate
+def comma_expr():
+    return {}
 
-def parse_optional_comma_expr(token_iter):
-    pass
 
+# , comma_expr                              ,
+# ε                                         )
+@parse_next_decorate
+def comma_exprs():
+    return {}
 
-def parse_comma_expr(token_iter):
-    pass
 
+# expr ; semicolon_exprs            if, while, {, let, case, new, (, ID, not, isvoid, ~, integer, string, true, false
+@parse_next_decorate
+def semicolon_expr():
+    return {}
 
-def parse_comma_exprs(token_iter):
-    pass
 
+# semicolon_expr	                if, while, {, let, case, new, (, ID, not, isvoid, ~, integer, string, true, false
+# ε                                 }
+@parse_next_decorate
+def semicolon_exprs():
+    return {}
 
-def parse_semicolon_expr(token_iter):
-    pass
 
+# ID <- not_term                            ID
+# not_term                                  not, isvoid, ~, ID, integer, string, true, false
+@parse_next_decorate
+def assign_term():
+    return {}
 
-def parse_semicolon_exprs(token_iter):
-    pass
 
+# not compare_term                          not
+# compare_term                              isvoid, ~, ID, integer, string, true, false
+@parse_next_decorate
+def not_term():
+    return {}
 
-def parse_assign_term(token_iter):
-    pass
 
+# add_term compare_term_rr                  isvoid, ~, ID, integer, string, true, false
+@parse_next_decorate
+def compare_term():
+    return {}
 
-def parse_not_term(token_iter):
-    pass
 
+# <= add_term compare_term_rr               <=
+# < add_term compare_term_rr                <
+# = add_term compare_term_rr                =
+# ε                                         @, ., ;, ,, then, else, fi, loop, pool, of, ), }, in
+@parse_next_decorate
+def compare_term_rr():
+    return {}
 
-def parse_compare_term(token_iter):
-    pass
 
+# multi_term add_term_rr                    isvoid, ~, ID, integer, string, true, false
+@parse_next_decorate
+def add_term():
+    return {}
 
-def parse_compare_term_rr(token_iter):
-    pass
 
+# + multi_term add_term_rr                  +
+# - multi_term add_term_rr                  -
+# ε                                         <=, <, =, @, ., ;, ,, then, else, fi, loop, pool, of, ), }, in
+@parse_next_decorate
+def add_term_rr():
+    return {}
 
-def parse_add_term(token_iter):
-    pass
 
+# isvoid_term multi_term_rr                 isvoid, ~, ID, integer, string, true, false
+@parse_next_decorate
+def multi_term():
+    return {}
 
-def parse_add_term_rr(token_iter):
-    pass
 
+# * isvoid_term multi_term_rr	            *
+# / isvoid_term multi_term_rr	            /
+# ε	                                        +, -, <=, <, =, @, ., ;, ,, then, else, fi, loop, pool, of, ), }, in
+@parse_next_decorate
+def multi_term_rr():
+    return {}
 
-def parse_multi_term(token_iter):
-    pass
 
+# isvoid tilde_term                         isvoid
+# tilde_term                                ~, ID, integer, string, true, false
+@parse_next_decorate
+def isvoid_term():
+    return {}
 
-def parse_multi_term_rr(token_iter):
-    pass
 
+# ~ factor                                  ~
+# factor                                    ID, integer, string, true, false
+@parse_next_decorate
+def tilde_term():
+    return {}
 
-def parse_isvoid_term(token_iter):
-    pass
 
+# ID factor_id                              ID
+# integer	                                integer
+# string	                                string
+# true	                                    TRUE
+# false                                     FALSE
+@parse_next_decorate
+def factor():
+    return {}
 
-def parse_tilde_term(token_iter):
-    pass
 
-
-def parse_factor(token_iter):
-    pass
-
-
-def parse_factor_id(token_iter):
-    pass
-
+# ( optional_comma_expr ) expr_rr           (
+# ε                                         *, /, +, -, <=, <, =, @, ., ;, ,, then, else, fi, loop, pool, of, ), }, in
+@parse_next_decorate
+def factor_id():
+    return {}
